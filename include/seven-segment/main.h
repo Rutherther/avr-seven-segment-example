f4db916a205b9613cb10a4ba6f7c5567f591b7d5
František Boháček <fandabohacek@gmail.com>
2020-09-20 08:58:44 +0200

    chore: initial commit

---
+++ include/seven-segment/main.h
@@ -0,0 +1,8 @@
+//
+// Created by ruther on 9/14/20.
+//
+
+#ifndef AVR_SEVEN_SEGMENT_EXAMPLE_MAIN_H
+#define AVR_SEVEN_SEGMENT_EXAMPLE_MAIN_H
+
+#endif //AVR_SEVEN_SEGMENT_EXAMPLE_MAIN_H
---
+++ src/main.c
@@ -0,0 +1,5 @@
+//
+// Created by ruther on 9/14/20.
+//
+
+#include "seven-segment/main.h"
